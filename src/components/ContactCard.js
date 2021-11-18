3cd39b15148680ff97252658809719db66a0b836
ochukoemmanuel <ochukoemmanuel07@gmail.com>
2021-11-18 11:40:13 +0100

    Create react components

---
+++ src/components/ContactCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CardContact = (props) => {
+    const {id, name, email} = props.contact;
+  return (
+    <div className="item">
+      <div className="content">
+        <div className="header">{name}</div>
+        <div className="header">{email}</div>
+      </div>
+      <i
+        className="trash alternate outline icon"
+        style={{ color: "red", marginTop: "7px" }}
+      ></i>
+    </div>
+  );
+};
+
+export default CardContact;
