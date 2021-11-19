74cddc312bcc420ecfa7bd2ac2a88f650132057d
ochukoemmanuel <ochukoemmanuel07@gmail.com>
2021-11-19 22:12:28 +0100

    update route(v6)

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { uuid } from 'uuidv4';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { uuid } from "uuidv4";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
@@ -11,7 +12,7 @@ function App() {
 
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts, {id: uuid(), ...contact}]);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
   };
 
   const removeContactHandler = (id) => {
@@ -20,11 +21,11 @@ function App() {
     });
 
     setContacts(newContactList);
-  }
+  };
 
   useEffect(() => {
-    const retriveContacts =JSON.parse( localStorage.getItem(LOCAL_STORAGE_KEY));
-    if(retriveContacts) setContacts(retriveContacts)
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
   }, []);
 
   useEffect(() => {
@@ -34,8 +35,16 @@ function App() {
   return (
     <div className="ui container">
       <Header />
-      <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} getContactId = {removeContactHandler} />
+      <Router>
+        <Routes>
+          <Route path="/" element={ContactList} />
+          <Route path="/add" element={AddContact} />
+        </Routes>
+      </Router>
+
+      {/* <AddContact addContactHandler={addContactHandler} />
+        <ContactList contacts={contacts} getContactId={removeContactHandler} />
+     */}
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -5,7 +5,15 @@ const ContactList = (props) => {
   const deleteContactHandler = (id) => {
     props.getContactId(id)
   };
-  const renderContactList = props.contacts.map((contact) => {
+
+  const contacts = [{
+    id: "1",
+    "name": "Dipesh",
+    "email": "cs@gmail.com",
+  },
+];
+
+  const renderContactList = contacts.map((contact) => {
     return (
       <ContactCard contact={contact} clickHandler={deleteContactHandler} key = {contact.id}></ContactCard>
     );
